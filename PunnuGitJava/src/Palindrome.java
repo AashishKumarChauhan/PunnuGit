eeea1f7971a4d09419fd3ebaf76b1e32215addd0
chauh <chauh@Bella>
2019-03-16 23:39:08 +0530

    created a palindrome.java file and written a code

---
+++ PunnuGitJava/src/Palindrome.java
@@ -0,0 +1,19 @@
+
+public class Palindrome {
+	public static void main(String[] args) {
+		int r,c=0,temp;
+		int n = 431;
+		temp=n;
+		while(n>0) {
+			r=n%10;
+			c=(c*10)+r;
+			n=n/10;
+		}
+		if(temp==c) {
+			System.out.println(temp +" is a Palindrome Number");
+		}
+		else{
+			System.out.println( temp + " is not a Palindrome Number ");
+		}  
+		}
+}
